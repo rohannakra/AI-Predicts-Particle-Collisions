37e941731ad41c5ed70fe98ee4d70fa85603cd79
RoJuan230 <rohan.nakra100@gmail.com>
2021-03-15 08:25:42 -0700

    Implementing quantum neural network

--- quantum_circuit.py
+++ quantum_circuit.py
@@ -45,6 +45,48 @@ class QuantumCircuit:
 
 simulator = qiskit.Aer.get_backend('qasm_simulator')
 
-circuit = QuantumCircuit(1, simulator, 100)
-print('Expected value for rotation pi {}'.format(circuit.run([np.pi])[0]))
-circuit._circuit.draw()
+
+class HybridFunction(Function):
+    """ Hybrid quantum - classical function definition """
+    
+    @staticmethod
+    def forward(ctx, input, quantum_circuit, shift):
+        """ Forward pass computation """
+        ctx.shift = shift
+        ctx.quantum_circuit = quantum_circuit
+
+        expectation_z = ctx.quantum_circuit.run(input[0].tolist())
+        result = torch.tensor([expectation_z])
+        ctx.save_for_backward(input, result)
+
+        return result
+        
+    @staticmethod
+    def backward(ctx, grad_output):
+        """ Backward pass computation """
+        input, expectation_z = ctx.saved_tensors
+        input_list = np.array(input.tolist())
+        
+        shift_right = input_list + np.ones(input_list.shape) * ctx.shift
+        shift_left = input_list - np.ones(input_list.shape) * ctx.shift
+        
+        gradients = []
+        for i in range(len(input_list)):
+            expectation_right = ctx.quantum_circuit.run(shift_right[i])
+            expectation_left  = ctx.quantum_circuit.run(shift_left[i])
+            
+            gradient = torch.tensor([expectation_right]) - torch.tensor([expectation_left])
+            gradients.append(gradient)
+        gradients = np.array([gradients]).T
+        return torch.tensor([gradients]).float() * grad_output.float(), None, None
+
+class Hybrid(nn.Module):
+    """ Hybrid quantum - classical layer definition """
+    
+    def __init__(self, backend, shots, shift):
+        super(Hybrid, self).__init__()
+        self.quantum_circuit = QuantumCircuit(1, backend, shots)
+        self.shift = shift
+        
+    def forward(self, input):
+        return HybridFunction.apply(input, self.quantum_circuit, self.shift)
